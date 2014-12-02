c27475e8e62afce6db3944040f08633f6d75868d
Pravin <pravin@occam.in>
2014-12-02 13:22:04 +0530

    GPK first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gpk::Application.config.secret_token = '2565f3272afda244f9fa8f76574c97edf0906b551dcce2af9cf9925de1d891f33170138cda92a268757cccbcc580221e41c52446ad1f2fc8df7e812ae1999d1e'
